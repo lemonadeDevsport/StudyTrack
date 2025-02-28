01576a32883e799ebf1997395bfcd724f6ffc0a9
lemonadeDevsport <sofiamdlsantos@gmail.com>
2025-02-28 12:46:36 -0500

    Commit fix CSS

--- action.js
+++ action.js
@@ -74,28 +74,6 @@ window.addEventListener('scroll', function () {
 
 
 
-// Contains the link for all social media handles
-var links = document.getElementsByClassName("social-media");
-
-links[0].addEventListener("click", () => { openlink(1) });
-links[1].addEventListener("click", () => { openlink(2) });
-links[2].addEventListener("click", () => { openlink(3) });
-links[3].addEventListener("click", () => { openlink(4) });
-
-function openlink(x) {
-    if (x == 1) {
-        window.open("https://www.instagram.com/_.vini._02_/", "_blank");
-    }
-    if (x == 2) {
-        window.open("https://www.linkedin.com/in/vineet-kumar-gupta-2833ab196/", "_blank");
-    }
-    if (x == 3) {
-        window.open("https://github.com/VineetKumar02", "_blank");
-    }
-    if (x == 4) {
-        window.open("https://vineet-portfolio-site.netlify.app/", "_blank");
-    }
-}
 
 
 async function searchAI() {
@@ -135,3 +113,12 @@ async function searchAI() {
 
     document.getElementById("search-input").value = "";
 }
+window.addEventListener('scroll', function() {
+    const parallaxElements = document.querySelectorAll('.parallax');
+    let scrollPosition = window.pageYOffset;
+
+    parallaxElements.forEach(function(element) {
+        let speed = element.getAttribute('data-speed');
+        element.style.transform = 'translateY(' + (scrollPosition * speed) + 'px)';
+    });
+});
